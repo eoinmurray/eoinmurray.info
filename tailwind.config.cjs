c9722cf2bb7190f30fcd69977f3d2148bc298007
Eoin Murray <me@eoinmurray.info>
2025-12-20 13:54:59 +0100

    Build minimal Vite site

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./about/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      letterSpacing: {
+        tightish: "-0.02em"
+      }
+    }
+  },
+  plugins: []
+};
